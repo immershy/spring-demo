3c719e368369dbf5355d9a454e064feb5e35f37d
zhengwei <zhengwei@u51.com>
2017-10-18 13:00:24 +0800

    update

---
+++ service0_1/src/main/java/cn/demo/service0/controller/Service0Controller.java
@@ -0,0 +1,24 @@
+package cn.demo.service0.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Leo
+ * @Blog: http://blog.csdn.net/lc0817
+ * @CreateTime: 2017/1/19 12:13
+ * @Description:
+ */
+@RestController
+public class Service0Controller {
+
+    @GetMapping("test/{province}")
+    String user(
+        @PathVariable String province
+    ) {
+        return "zhengjiang___"+province;
+    }
+
+
+}
